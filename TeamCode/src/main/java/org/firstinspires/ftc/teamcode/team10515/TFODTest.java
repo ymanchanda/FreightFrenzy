241df92bc8e4c46e872429427791997eba49cd26
ymanchanda <ymanchanda@gmail.com>
2022-01-13 20:30:20 -0500

    TFOD updated (not tested yet) Changed creation of detector and list Changed camera stream resolution Moved instantiation of detector to init()

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team10515/TFODTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team10515/TFODTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.R;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
@@ -27,21 +28,24 @@ public class TFODTest extends FreightFrenzyRobot {
     /* Declare OpMode members. */
 
     OpenCvWebcam webcam;
-    SamplePipeline pipeline = new SamplePipeline();
+    SamplePipeline pipeline;
+    TensorObjectDetector tfod;
+    List<TensorObjectDetector.Detection> detections;
 
     @Override
     public void init(){
         super.init();
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", String.valueOf(R.id.cameraMonitorViewId), hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
 
+        pipeline = new SamplePipeline();
         webcam.setPipeline(pipeline);
 
         webcam.setMillisecondsPermissionTimeout(2500);
         webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened() {
-                webcam.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+                webcam.startStreaming(640, 480, OpenCvCameraRotation.UPRIGHT);
             }
 
             @Override
@@ -50,6 +54,14 @@ public class TFODTest extends FreightFrenzyRobot {
                 telemetry.update();
             }
         });
+
+        try {
+            tfod = new TFODBuilder(hardwareMap, "model2.lite", "element").build();
+            telemetry.addLine("Successful build of model");
+        } catch (IOException e) {
+            e.printStackTrace();
+            telemetry.addLine("Model build failed");
+        }
 
         /*
          * Initialize the drive system variables.
@@ -75,20 +87,21 @@ public class TFODTest extends FreightFrenzyRobot {
             telemetry.addLine("A Pressed");
             webcam.stopStreaming();
         }
+        else if(getEnhancedGamepad1().isB()){
+            telemetry.addLine("B Pressed");
+            webcam.startStreaming(640, 480);
+        }
         else if (getEnhancedGamepad1().isY()){
-            try {
-                telemetry.addLine("Y Pressed");
-                TensorObjectDetector tfod = new TFODBuilder(hardwareMap, "model2.lite", "element").build();
-                List<TensorObjectDetector.Detection> detections = tfod.recognize(pipeline.getMat());
-                telemetry.addLine("Size of list: " + String.valueOf(detections.size()));
-                telemetry.addLine("Left most coordinate of bounding box: " + String.valueOf(detections.get(0).getLocation().left));
-                telemetry.addLine("Confidence: " + String.valueOf(detections.get(0).getConfidence()));
-                while(!getEnhancedGamepad1().isB()){
-                    telemetry.addLine("Press B to continue");
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+            telemetry.addLine("Y Pressed");
+            detections = tfod.recognize(pipeline.getMat());
+            telemetry.addLine("Size of list: " + detections.size());
+            telemetry.addLine("Left most coordinate of bounding box: " + detections.get(0).getLocation().left);
+            telemetry.addLine("Confidence: " + detections.get(0).getConfidence());
+            while(!getEnhancedGamepad1().isB()){
+                telemetry.addLine("Press B to continue");
+                telemetry.update();
             }
+
         }
         telemetry.update();
 //            sleep(100);
